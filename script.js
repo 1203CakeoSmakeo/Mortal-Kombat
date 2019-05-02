364210890bf3ab27d3b6f02f86f8000e4e2e8c37
1203CakeoSmakeo <43444272+1203CakeoSmakeo@users.noreply.github.com>
2019-05-02 09:51:27 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+var ProductNames = [];
+var StorePrices = [];
+$("button").click(function() {
+    var Blue = $("#Productnames").val();
+    console.log(Blue);
+});
+var StorePrices = [];
+var StorePrices = [];
+$("button").click(function() {
+    var Blue = $("#StorePrices").val();
+    console.log(Blue);
+});
